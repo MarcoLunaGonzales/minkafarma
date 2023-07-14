c258c507327c44fb974a915f274baf53bdd9ae48
roalxDcar <roalmollericona@gmail.com>
2023-07-14 00:59:24 -0400

    copiar precio cliente

--- programas/clientes/prgListaClientes.php
+++ programas/clientes/prgListaClientes.php
@@ -61,9 +61,12 @@ $globalAlmacen=$_COOKIE["global_almacen"];
             <a href='../../clientePrecio.php?cod_cliente=<?php echo $codCliente; ?>' title='Registrar Precios Clientes' class='text-dark'><i class='material-icons'>description</i></a>
             <a href='../../clienteDocumento.php?cod_cliente=<?php echo $codCliente; ?>' target='_blank' title='Carga de Documentos' class='text-dark'><i class='material-icons'>cloud_upload</i></a>
             <a href='#' title='Carga de Precio Clientes' class='text-primary modal_documento' data-cod_cliente="<?php echo $codCliente; ?>">
-                <i class='material-icons'>description</i>
+              <i class='material-icons'>description</i>
             </a>
             <a href="../../clientePrecioVer.php?cod_cliente=<?php echo $codCliente; ?>" title="Ver Precios Clientes" class="text-info"><i class="material-icons">visibility</i></a>
+            <a href='#' title='Clonar Precio Clientes' class='text-warning modal_clonar' data-cod_cliente="<?php echo $codCliente; ?>">
+              <i class='material-icons'>file_copy</i>
+            </a>
           </td>
         </tr>
         <?php
@@ -92,7 +95,44 @@ $globalAlmacen=$_COOKIE["global_almacen"];
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" onClick="$('#cargarModal').modal('hide');">Cancelar</button>
-                <button id="cargar_save" class="btn btn-primary cargar_save">Guardar</button>
+                <button id="cargar_save" class="btn btn-primary">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal Carga de Datos -->
+<div class="modal fade" id="clonarModal" tabindex="-1" aria-labelledby="clonarModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="clonarModalLabel">Copiar - Precio Cliente</h5>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <input type="hidden" id="cliente_actual">
+                    <label for="cliente_clonado">Clonar de cliente:</label>
+                    <select id="cliente_clonado" class="form-control select2">
+                        <?php
+                        $consulta=" SELECT c.cod_cliente, CONCAT(c.nombre_cliente, ' ', c.paterno) as nombre_cliente
+                              FROM clientes AS c INNER JOIN ciudades AS a ON c.cod_area_empresa = a.cod_ciudad 
+                              WHERE c.cod_area_empresa='$globalAgencia' ORDER BY c.nombre_cliente ASC
+                          ";
+                          $rs = mysqli_query($enlaceCon,$consulta);
+                          while($row=mysqli_fetch_array($rs))
+                          {
+                        ?>
+                            <option value="<?=$row['cod_cliente']?>"><?=$row['nombre_cliente']?></option>
+                        <?php
+                          }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" onClick="$('#clonarModal').modal('hide');">Cancelar</button>
+                <button id="clonar_save" class="btn btn-primary">Guardar</button>
             </div>
         </div>
     </div>
