a44648fafbfec44b06b672ef2c5f3f5c6c052369
Ronald <45970198+roalxDcar@users.noreply.github.com>
2023-12-07 17:59:06 -0400

    reporte de ingresos

--- rpt_op_inv_ingresos.php
+++ rpt_op_inv_ingresos.php
@@ -1,4 +1,6 @@
 <?php
+$global_usuario = $_COOKIE['global_usuario'];
+$global_agencia = $_COOKIE['global_agencia']; 
 echo "<script language='JavaScript'>
 		function envia_formulario(f)
 		{	var rpt_territorio,rpt_almacen, tipo_ingreso,fecha_ini, fecha_fin, rpt_item;
@@ -18,12 +20,10 @@ echo "<script language='JavaScript'>
 		}
 		</script>";
 require("conexion.inc");
-if($global_tipoalmacen==1)
-{	require("estilos_almacenes_central.inc");
-}
-else
-{	require("estilos_almacenes.inc");
-}$fecha_rptdefault=date("Y-m-d");
+
+require("estilos_almacenes.inc");
+
+$fecha_rptdefault=date("Y-m-d");
 echo "<h1>Reporte Ingresos Almacen</h1>";
 
 echo"<form method='post' action=''>";
@@ -35,9 +35,9 @@ echo"<form method='post' action=''>";
 	else
 	{	$sql="select cod_ciudad, descripcion from ciudades where cod_ciudad='$global_agencia' order by descripcion";
 	}
-	$resp=mysql_query($sql);
+	$resp=mysqli_query($enlaceCon, $sql);
 	echo "<option value=''></option>";
-	while($dat=mysql_fetch_array($resp))
+	while($dat=mysqli_fetch_array($resp))
 	{	$codigo_ciudad=$dat[0];
 		$nombre_ciudad=$dat[1];
 		if($rpt_territorio==$codigo_ciudad)
@@ -50,8 +50,8 @@ echo"<form method='post' action=''>";
 	echo "</select></td></tr>";
 	echo "<tr><th align='left'>Almacen</th><td><select name='rpt_almacen' class='texto'>";
 	$sql="select cod_almacen, nombre_almacen from almacenes where cod_ciudad='$rpt_territorio'";
-	$resp=mysql_query($sql);
-	while($dat=mysql_fetch_array($resp))
+	$resp=mysqli_query($enlaceCon, $sql);
+	while($dat=mysqli_fetch_array($resp))
 	{	$codigo_almacen=$dat[0];
 		$nombre_almacen=$dat[1];
 		if($rpt_almacen==$codigo_almacen)
@@ -65,10 +65,10 @@ echo"<form method='post' action=''>";
 
 	echo "<tr><th align='left'>Tipo de Ingreso</th>";
 	$sql_tipoingreso="select cod_tipoingreso, nombre_tipoingreso from tipos_ingreso order by nombre_tipoingreso";
-	$resp_tipoingreso=mysql_query($sql_tipoingreso);
+	$resp_tipoingreso=mysqli_query($enlaceCon, $sql_tipoingreso);
 	echo "<td><select name='tipo_ingreso' class='texto'>";
 	echo "<option value=''>Todos los tipos</option>";
-	while($datos_tipoingreso=mysql_fetch_array($resp_tipoingreso))
+	while($datos_tipoingreso=mysqli_fetch_array($resp_tipoingreso))
 	{	$codigo_tipoingreso=$datos_tipoingreso[0];
 		$nombre_tipoingreso=$datos_tipoingreso[1];
 		if($tipo_ingreso==$codigo_tipoingreso)
@@ -82,9 +82,9 @@ echo"<form method='post' action=''>";
 
 	echo "<tr><th align='left'>Material</th><td><select name='rpt_item' class='texto'>";	
 	$sql_item="select codigo_material, descripcion_material from material_apoyo where codigo_material<>0 order by descripcion_material";	
-	$resp=mysql_query($sql_item);
+	$resp=mysqli_query($enlaceCon, $sql_item);
 	echo "<option value=''></option>";
-	while($dat=mysql_fetch_array($resp))
+	while($dat=mysqli_fetch_array($resp))
 	{	$codigo_item=$dat[0];
 		if($tipo_item==1)
 		{	$nombre_item="$dat[1] $dat[2]";
